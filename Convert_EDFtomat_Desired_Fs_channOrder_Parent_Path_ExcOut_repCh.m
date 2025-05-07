897eaf02b316ea1c083cbc4814372c3e05e29c74
venusmgi <mostaghs@uci.edu>
2025-05-07 14:16:46 -0700

    added more helper functions; WIP

--- Convert_EDFtomat_Desired_Fs_channOrder_Parent_Path_ExcOut_repCh.m
+++ Convert_EDFtomat_Desired_Fs_channOrder_Parent_Path_ExcOut_repCh.m
@@ -4,8 +4,26 @@
 % in "edfreadUntilDone.m"
 
 function Convert_EDFtomat_Desired_Fs_channOrder_Parent_Path_ExcOut_repCh (desired_sampling_rate,desired_channel_order,removable_channels, channel_tobe_replaced, new_channel_names,chann_info_name,header_format)
-% Check if the header format is EEGlab-compatible
-% Check if the header_format is provided and not empty
+% CONVERT_EDF2MAT Converts a single EDF file to MAT format with specified parameters
+%
+% This function converts an EDF file to MAT format, reordering channels and
+% resampling the data according to specified parameters.
+%
+% Inputs:
+%   file_path - Path to the EDF file
+%   file_name - Name of the EDF file
+%   desired_sampling_rate - Target sampling rate for the output
+%   desired_channel_order - Cell array of channel names in desired order
+%   removable_channels - Cell array of channels to be excluded
+%   channel_tobe_replaced - Cell array of channel names to be replaced
+%   new_channel_names - Cell array of new names for channels to be replaced
+%   header_format - 'EEGlab' for EEGlab format, empty for default format
+%   ParentPath - Parent directory path
+%   chann_info_name - Name for the channel information output file
+%   edf_name - Name of the EDF file being processed
+%
+% Outputs:
+%   Saves a .mat file with either EEGlab format or standard format
 
 functionPath = pwd;
 addpath(functionPath)
@@ -36,68 +54,74 @@ fprintf('All files processed successfully.\n');
    
 end
 
-%taking all the files that end with .edf
 
 
 
 
 function Convert_EDF2Mat (file_path,file_name,desired_sampling_rate,desired_channel_order,removable_channels,channel_tobe_replaced, new_channel_names,header_format,ParentPath,chann_info_name,edf_name)
+% CONVERT_EDF2MAT Converts an EDF file to MAT format with specified parameters
+%
+% This function reads an EDF file, processes it according to the specified parameters,
+% and saves the results in MAT format.
+%
+% Inputs:
+%   folder_path - Path to the folder containing the EDF file
+%   edf_name - Name of the EDF file
+%   desired_sampling_rate - Target sampling rate
+%   desired_channel_order - Cell array of channel names in desired order
+%   removable_channels - Cell array of channels to be excluded
+%   channel_tobe_replaced - Cell array of channel names to be replaced
+%   new_channel_names - Cell array of new names for channels to be replaced
+%   header_format - 'EEGlab' for EEGlab format, empty for default format
+%   ParentPath - Parent directory path
+%   chann_info_name - Name for the channel information output file
+%   edf_name - Name of the EDF file (for logging)
+
 
 addpath(file_path)
-
 isEEGlabFormat = strcmpi(header_format, 'EEGlab');
-num_chann = length(desired_channel_order);
+num_channels = length(desired_channel_order);
 [hdr, record] = edfreadUntilDone(string(file_name));
 EEG_original_channel_order = hdr.label;
 %     True_Fs = hdr.samples(1)/ hdr.duration;
-original_Fs = hdr.frequency(1);
+original_Fs = int64(hdr.frequency(1));  %TODO: figure out a better way to find the original fs
 
-
-%% removing the channels that have similarity in names with our main 19 channels
 EEG_record = record;
 clear record
 
-[reordered_record,sorted_channels_Indices] =Get_desired_channel_order_excelOutput_replacing_input (desired_channel_order,removable_channels,channel_tobe_replaced, new_channel_names,EEG_record, EEG_original_channel_order,ParentPath,chann_info_name,edf_name);
+%% removing the channels that have similarity in names with our main 19 channels
+
+
+[reordered_record,sorted_channels_Indices] =Get_desired_channel_order_excelOutput_replacing_input (desired_channel_order,removable_channels,channel_tobe_replaced, ...
+    new_channel_names,EEG_record, EEG_original_channel_order,ParentPath,chann_info_name,edf_name);
+
+
+
+% Determine output filename based on resampling
+fname = Get_Output_Filename(file_name, original_Fs, desired_sampling_rate);
+reordered_EEG.setname = fname;
+
+
+% Resample if needed
+if original_Fs ~= desired_sampling_rate
+    reordered_record = Resample_Data(reordered_record,  desired_sampling_rate, original_Fs, num_channels);
+    hdr.frequency(1:num_channels) = desired_sampling_rate;
+end
+
+%in case we wanted it to be in EEGLab format
 % Saving the EEG record of the channel order we want
 reordered_EEG = make_EEG_header(hdr,desired_channel_order);
 reordered_EEG.data = reordered_record;
 
 
-if original_Fs ~= desired_sampling_rate
-    extention = 'reordered_resampled_EEG';
-    fname = strcat(file_name(1:end-4),'_reordered_resampled.mat');
 
-else
-    extention = 'reordered_EEG';
-    fname = strcat(file_name(1:end-4),'_reordered.mat');
+% Save the processed data
+fprintf('Saving processed data...\n');
+Save_Processed_Data(fname, isEEGlabFormat, reordered_EEG, hdr, ...
+    sorted_channels_Indices, desired_channel_order, reordered_record);
 
-end
-reordered_EEG.setname = fname;
+fprintf('Successfully saved: %s\n', fname);
 
-
-
-reordered_record_old = reordered_record;
-clear reordered_record
-
-original_Fs = int64(original_Fs);
-for i = 1:num_chann
-    reordered_record(i,:) = resample(reordered_record_old(i,:) ,desired_sampling_rate,original_Fs);
-end
-
-hdr.frequency(1:num_chann) = desired_sampling_rate;
-
-
-% Saving the EEG record of the channel order we want
-if isEEGlabFormat
-    reordered_EEG = make_EEG_header_EEGlab(hdr, desired_channel_order);
-
-    save(fname, 'reordered_EEG', '-v7.3');
-else
-    reordered_hdr = new_header(hdr, sorted_channels_Indices, desired_channel_order);
-    save(fname,"reordered_record","reordered_hdr", '-v7.3');
-end
-
-display(strcat('done saving ',fname))
 
 clear EEG_original_channel_order EEG_record reordered_record hdr reordered_hdr reordered_EEG fname file_name file_name
 
@@ -203,4 +227,58 @@ for j = 1:length(edf_files)
         fprintf('Error processing %s: %s\n', edf_name, ME.message);
     end
 end
+end
+
+
+
+function fname = Get_Output_Filename(file_name, original_Fs, desired_sampling_rate)
+    % GETOUTPUTFILENAME Determines the output filename based on resampling
+    if original_Fs ~= desired_sampling_rate
+        fname = strcat(file_name(1:end-4), '_reordered_resampled.mat');
+    else
+        fname = strcat(file_name(1:end-4), '_reordered.mat');
+    end
+end
+
+function resmapledEEG = Resample_Data(EEG_signal,desired_sampling_rate,original_Fs,num_channels)
+    % RESAMPLEDATA Resamples the EEG data to the desired sampling rate
+    %
+    % Inputs:
+    %   EEG_signal - Original EEG data matrix (channels x time points)
+    %   original_Fs - Original sampling rate
+    %   desired_sampling_rate - Target sampling rate
+    %   num_channels - Number of channels
+    %
+    % Outputs:
+    %   resampledEEG - Resampled EEG data matrix with length adjusted for new sampling rate
+    
+    %% Resample first channel to get the exact output length
+    temp_resampled = resample(EEG_signal(1,:), desired_sampling_rate, original_Fs);
+    new_length = length(temp_resampled);
+        
+    % Pre-allocate output array with the exact length from resample
+    resampledEEG = zeros(num_channels, new_length);
+    
+    % Store the first channel's resampled data
+    resampledEEG(1,:) = temp_resampled;
+    
+    % Resample remaining channels
+    for i = 2:num_channels
+        resampledEEG(i,:) = resample(EEG_signal(i,:), desired_sampling_rate, original_Fs);
+    end
+end
+
+function Save_Processed_Data(fname,isEEGlabFormat,reordered_EEG,hdr,...
+                              sorted_channels_Indices,desired_channel_order,reordered_record)
+    % Saving the EEG record of the channel order we want
+    if isEEGlabFormat
+        reordered_EEG = make_EEG_header_EEGlab(hdr, desired_channel_order);
+    
+        save(fname, 'reordered_EEG', '-v7.3');
+    else
+        reordered_hdr = new_header(hdr, sorted_channels_Indices, desired_channel_order);
+        save(fname,"reordered_record","reordered_hdr", '-v7.3');
+    end
+
+
 end
