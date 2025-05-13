e0457388c74ac826f654562aeda3a0a277cbdb8f
venusmgi <mostaghs@uci.edu>
2025-05-12 18:58:15 -0700

    Applied changes;WIP

--- Resolve_Channel_Duplicate.m
+++ Resolve_Channel_Duplicate.m
@@ -31,57 +31,47 @@ channelCounters = struct('EMG', 0, 'EKG', 0, 'Fz', 0, 'O1', 0, 'O2', 0);
 allChannelLabels = Handle_Difficult_Channel (allChannelLabels);
 
 
-%making sure there will not be multipe EMG1, EMG2, EKG1, and EKG2
-%becuase if first we encounter a channel with EMG or EKG in the name, but it is
-%not out desired channel, that one willbe named EMG1, or EKG1
 
-%instead of this I could have started EMG and EKG numbers from 2 instead of
-% 0 
-
-channs = allChannelLabels(contains(allChannelLabels,'correct_'));
-if any(contains(channs,'EMG'))
-    channelCounters.('EMG') = sum(contains(allChannelLabels,'EMG'));
-elseif any(contains(channs,'EKG'))
-    channelCounters.('EKG') = sum(contains(allChannelLabels,'EKG'));
+% Removing all the channels that had similarity in names but were not
+% selected by the user
+for i = 1:length(channelNames)
+    channelName = channelNames{i};
+    idxToInvestigate = find(contains(allChannelLabels,channelName));
+    userSelectedIdx = find(contains(allChannelLabels,'correct_'));
+    idxChannelToRemove = setdiff(idxToInvestigate,userSelectedIdx); %fiding channels that have our desired pattern but are not the correct channels
+    if ~isempty(idxChannelToRemove)
+        allChannelLabels{idxChannelToRemove} = 'useless';
+    end
 end
 
 
+% Loop through each channel name to check if the current label matches
+% any known types and count them
 
-% Iterate through the channel names to standardize and count them
+aa = allChannelLabels;
 for j = 1:length(allChannelLabels)
     channelLabel = allChannelLabels{j};
-    % Loop through each channel name to check if the current label matches any known types
-    for i = 1:length(channelNames)
-        channelName = channelNames{i};
-    
+
+    for k = 1:length(channelNames)
+        channelName = channelNames{k};
+
         if contains(lower(channelLabel),lower(channelName))
-            % Update the counter for the channel
-            channelCounters.(channelName) = channelCounters.(channelName) + 1;
-            % For the main EEG channels ('O1', 'O2', 'Fz'), preserve the
-            % base name without adding a number, as they are standard electrode names
-            if strcmp(channelLabel,'correct_O1') || strcmp(channelLabel,'correct_O2') ||strcmp(channelLabel,'correct_Fz')
-                 newLabel = channelName; % Preserve the base name without number for the correct instance
-            elseif strcmp(channelLabel,'correct_EMG1') 
-                newLabel = 'EMG1';
-                elseif strcmp(channelLabel,'correct_EMG2') 
-                newLabel = 'EMG2';
-                elseif strcmp(channelLabel,'correct_EKG1') 
-                newLabel = 'EKG1';
-                elseif strcmp(channelLabel,'correct_EKG2') 
-                newLabel = 'EKG2';
-            else
-                %% We don't need extra channels then keep the correct ones
-                % Append a numeral suffix for duplicate or non-main channels
-                newLabel = [channelName num2str(channelCounters.(channelName))];
-            end
+            channelCounters.(channelName) = channelCounters.(channelName)+1;
 
+            if strcmp(channelLabel,'correct_O1') || strcmp(channelLabel,'correct_O2') ||strcmp(channelLabel,'correct_Fz')
+                newLabel = channelName; % Preserve the base name without number for the correct instance
+            else
+
+               newLabel = [channelName num2str(channelCounters.(channelName))];
+            end
             allChannelLabels{j} = newLabel;
-            
         end
-    end
 
     
+    end
 end
+
+
 % Return the unchanged label if no matching channel name is found
 outputChannelName = allChannelLabels;
 
@@ -89,22 +79,36 @@ outputChannelName = allChannelLabels;
 
 end
 
-function [outPutChannelLabels] = Handle_Difficult_Channel (allChannelLabels)
+% % function [outPutChannelLabels,channelCounters] = Handle_Difficult_Channel (allChannelLabels,channelCounters)
 
-difficultChannelNames = {'O1','O2','Fz','EMG1','EMG2','EKG1', 'EKG2'};
+function [outPutChannelLabels] = Handle_Difficult_Channel (allChannelLabels)
+difficultChannelNames = {'O1','O2','Fz','EMG','EKG'};
 for i = 1:length(difficultChannelNames)
     channelName = difficultChannelNames{i};
     channelOptions = allChannelLabels(contains(lower(allChannelLabels),lower(channelName)));
-   if ~isempty(channelOptions)
-        if isscalar(channelOptions) %if we have one option, that is the correct one
+    uniqueChannelOptions = unique(channelOptions);
+
     
-            allChannelLabels (strcmp(allChannelLabels,channelOptions)) = {['correct_' channelName]};
+   if ~isempty(uniqueChannelOptions)
+       
+        if isscalar(uniqueChannelOptions) %if we have one option, that is the correct one
+            % % %adding channel counters
+            % % numChannelsCounted = sum(strcmp(allChannelLabels,uniqueChannelOptions));
+            % % channelCounters.(channelName) = channelCounters.(channelName) + numChannelsCounted;
+
+            allChannelLabels (strcmp(allChannelLabels,uniqueChannelOptions)) = {['correct_' channelName]};
+            
+
         else % if we have more than one option, ask the user
     
         selection = questdlg(['Which one of these ', channelName, ...
             ' channels is in the main 19 EEG electrodes?'],...
-            'Select Main EEG Electrode', channelOptions{:},channelOptions{1});
-            allChannelLabels (strcmp(allChannelLabels,selection)) = {['correct_' channelName]};
+            'Select the Correct Name,(hint: if it is not one of the main EEG electrodes, and you have multiple of the same name, just pick one!)', channelOptions{:},channelOptions{1});
+        
+        % % %adding channel counters
+        % % numChannelsCounted = sum(strcmp(allChannelLabels,selection));
+        % % channelCounters.(channelName) = channelCounters.(channelName) + numChannelsCounted;
+        allChannelLabels (strcmp(allChannelLabels,selection)) = {['correct_' channelName]};
         end
    end
             
